a1c5d25d51f5986db024f3d2e3424f489d5fdad6
Leandro Peres <leandroperes@protonmail.com>
2024-08-19 15:55:15 -0300

    Clearmem makefile

--- Intro/clearmem/clearmem.asm
+++ Intro/clearmem/clearmem.asm
@@ -2,7 +2,6 @@
 ;; The iNES header (contains a total of 16 bytes with the flags at $7F00)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .segment "HEADER"
-.org $7F00
 .byte $4E,$45,$53,$1A        ; 4 bytes with the characters 'N','E','S','\n'
 .byte $02                    ; How many 16KB of PRG-ROM we'll use (=32KB)
 .byte $01                    ; How many 8KB of CHR-ROM we'll use (=8KB)
@@ -17,32 +16,47 @@
 ;; PRG-ROM code located at $8000
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .segment "CODE"
-.org $8000
 
 RESET:
-    sei                      ; Disable all IRQ interrupts
-    cld                      ; Clear the decimal mode (unsupported by the NES)
-    ldx #$FF
-    txs                      ; Initialize the stack pointer at $01FF
+    SEI                      ; Disable all IRQ interrupts
+    CLD                      ; Clear the decimal mode (unsupported by the NES)
+    LDX #$FF
+    TXS                      ; Initialize the stack pointer at $01FF
 
-    lda #$0                  ; A = 0
-    inx
-MemLoop:
-    sta $0,x                 ; Store the value of A (zero) into {$0 + X}
-    dex                      ; X--
-    bne MemLoop              ; If X is not zero, we loop back to MemLoop
-    
+    INX                      ; Increment X, causing it to roll-off from $FF to $00
+    TXA                      ; A = 0
+
+ClearRAM:
+    STA $0000,x              ; Store the value of A (zero) from $0000 to $00FF
+    STA $0100,x              ; Store the value of A (zero) from $0100 to $01FF
+    STA $0200,x              ; Store the value of A (zero) from $0200 to $02FF
+    STA $0300,x              ; Store the value of A (zero) from $0300 to $03FF
+    STA $0400,x              ; Store the value of A (zero) from $0400 to $04FF
+    STA $0500,x              ; Store the value of A (zero) from $0500 to $05FF
+    STA $0600,x              ; Store the value of A (zero) from $0600 to $06FF
+    STA $0700,x              ; Store the value of A (zero) from $0700 to $07FF
+    DEX                      ; X--
+    BNE ClearRAM             ; If X is not zero, we loop back to ClearRAM
+
+LoopForever:
+    JMP LoopForever
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NMI Interrupt handler
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 NMI:
-    rti                      ; Return from interrupt
+    RTI                      ; Return from interrupt
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; IRQ Interrupt handler
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 IRQ:
-    rti                      ; Return from interrupt
+    RTI                      ; Return from interrupt
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Vectors with the addresses of the handlers that we always add at $FFFA
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .segment "VECTORS"
-.org $FFFA
 .word NMI                    ; Address (2 bytes) of the NMI handler
 .word RESET                  ; Address (2 bytes) of the Reset handler
 .word IRQ                    ; Address (2 bytes) of the IRQ handler
