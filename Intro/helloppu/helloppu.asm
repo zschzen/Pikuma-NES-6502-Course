292929da7310d6ceed65a1c5fe93c6eab32c6c11
Leandro Peres <leandroperes@protonmail.com>
2024-08-21 18:45:44 -0300

    Color pallet

--- Intro/helloppu/helloppu.asm
+++ Intro/helloppu/helloppu.asm
@@ -14,16 +14,22 @@ Reset:
   INIT_NES
 
 Main:
+    BIT PPU_STATUS           ; Read PPU_STATUS to reset the PPU_ADDR Latch
     LDX #$3F
     STX PPU_ADDR             ; Set hi-byte of PPU_ADDR to $3F
     LDX #$00
     STX PPU_ADDR             ; Set lo-byte of PPU_ADDR to $00
 
-    LDA #$04
-    STA PPU_DATA             ; Send $04 to PPU_DATA
+    LDY #0
+LoopPalette:
+    LDA PaletteData,y        ; Lookup byte in ROM
+    STA PPU_DATA             ; Set value to send to PPU_DATA
+    INY                      ; Y++
+    CPY #32                  ; Y == 32
+    BNE LoopPalette          ; Not equal, keep looping
 
     LDA #%00011110
-    STA PPU_MASK             ; Set PPU_MASK bits to show background and sprites
+    STA PPU_MASK             ; Set PPU_MASK bits to show background
 
 LoopForever:
     JMP LoopForever
@@ -39,4 +45,8 @@ NMI:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 IRQ:
     RTI                      ; Return from interrupt
+
+PaletteData:
+.byte $0F,$2A,$0C,$3A, $0F,$2A,$0C,$3A, $0F,$2A,$0C,$3A, $0F,$2A,$0C,$3A ; Background
+.byte $0F,$10,$00,$26, $0F,$10,$00,$26, $0F,$10,$00,$26, $0F,$10,$00,$26 ; Sprites
 
